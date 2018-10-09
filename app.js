636672b86dc9e3743f29081f46bdafc32e218d88
abdelkrimn <abdelkrim.n@codeur.online>
2018-10-09 16:21:16 +0200

    iniy

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const random = Math.round(Math.random() * 100);
 const myButton = document.querySelector('.btn');
 var input = document.querySelector('#answer');
-var str = "";
+var i = 0;
 
 myButton.addEventListener('click', (event) => {
-    console.log(myButton);
-    console.log(random);
-    console.log(input.value);
+    // event.preventDefault();
+    i++;
     console.log(i);
     if (input.value == random) {
         alert("Bravo");
-    } else if (input.value < random) {
+    } 
+    else if (input.value < random) {
         alert("plus");
-    } else if (input.value > random) {
+    } 
+    else if (input.value > random) {
         alert("moins");
     }
-    for (var i = 0; i < 9; i++) {
-        str = str + i;
-        alert("fini");
+    if(i == 3){
+        alert("perdu");
+        i=0;
     }
 })
