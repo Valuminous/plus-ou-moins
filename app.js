cf035802327a081ad43b16fc0e660e80f78c9ea1
Unknown <valerie.h@codeur.online>
2018-10-09 15:03:25 +0200

    [JS]
    (Script)

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+const random = Math.round(Math.random() * 100);
+const myButton = document.querySelector('.btn');
+var input = document.querySelector('#answer');
+var str = "";
 
-for(var i=0; i<300; i++){
-    const random = Math.round(Math.random()*100);
+myButton.addEventListener('click', (event) => {
+    console.log(myButton);
     console.log(random);
-}
+    console.log(input.value);
+    console.log(i);
+    if (input.value == random) {
+        alert("Bravo");
+    } else if (input.value < random) {
+        alert("plus");
+    } else if (input.value > random) {
+        alert("moins");
+    }
+    for (var i = 0; i < 9; i++) {
+        str = str + i;
+        alert("fini");
+    }
+})
