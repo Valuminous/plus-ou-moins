e99e20c8134b6cb7545c424d1c079408bbfc3b90
Unknown <valerie.h@codeur.online>
2018-10-09 16:20:16 +0200

    [JS]
    update

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const random = Math.round(Math.random() * 100);
 const myButton = document.querySelector('.btn');
 var input = document.querySelector('#answer');
-var str = "";
+var i = 0;
 
 myButton.addEventListener('click', (event) => {
-    console.log(myButton);
+    i++;
     console.log(random);
-    console.log(input.value);
+    console.log(myButton);
     console.log(i);
+    console.log(input);
     if (input.value == random) {
         alert("Bravo");
+        input.value="";
     } else if (input.value < random) {
         alert("plus");
+        input.value="";
     } else if (input.value > random) {
         alert("moins");
+        input.value="";
     }
-    for (var i = 0; i < 9; i++) {
-        str = str + i;
-        alert("fini");
-    }
+     if (i == 4) {
+         alert("salut");
+         i=0;
+     }
 })
