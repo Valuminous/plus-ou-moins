cded595e3dca216392eaed1a50551ecfe48a9a8e
abdelkrimn <abdelkrim.n@codeur.online>
2018-10-09 20:00:48 +0200

    [JS]
    regex

--- app.js
+++ app.js
@@ -1,46 +1,38 @@
-const random = Math.round(Math.random() * 100);
+const random = Math.floor(Math.random() * 100);
 const myButton = document.querySelector('.btn');
 var input = document.querySelector('#answer');
 var i = 0;
 
+
+ var regex = /^\d+$/;
+
 myButton.addEventListener('click', (event) => {
-<<<<<<< HEAD
-    // event.preventDefault();
     i++;
-=======
-    i++;
-    console.log(random);
+   
+    if (regex.test(input.value)){}
+        
+    else{
+         alert("Ce n'est pas un nombre valide");
+         i--;
+    }
+       
     console.log(myButton);
->>>>>>> master
     console.log(i);
     console.log(input);
+
+    if (i == 3) {
+        alert("salut");
+        window.location.reload();
+        input.value = "";
+    }
     if (input.value == random) {
         alert("Bravo");
-<<<<<<< HEAD
-    } 
-    else if (input.value < random) {
-        alert("plus");
-    } 
-    else if (input.value > random) {
-=======
-        input.value="";
+        input.value = "";
     } else if (input.value < random) {
         alert("plus");
-        input.value="";
+        input.value = "";
     } else if (input.value > random) {
->>>>>>> master
         alert("moins");
-        input.value="";
+        input.value = "";
     }
-<<<<<<< HEAD
-    if(i == 3){
-        alert("perdu");
-        i=0;
-    }
-=======
-     if (i == 4) {
-         alert("salut");
-         i=0;
-     }
->>>>>>> master
 })
