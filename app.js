00f1f4b9dd3f29d3462087c0fcf4514ab262d1ac
Unknown <valerie.h@codeur.online>
2018-10-10 11:39:50 +0200

    [JS, HTML, CSS]
    (nouvelle version)

--- app.js
+++ app.js
@@ -1,37 +1,47 @@
 const random = Math.floor(Math.random() * 100);
-const myButton = document.querySelector('.btn');
+const myButton = document.querySelector('#btn');
 var message = document.querySelector('#message');
+var essais = document.querySelector("#essais");
 var input = document.querySelector('#answer');
 var i = 0;
+var regex = /^\d+$/;
 
-
- var regex = /^\d+$/;
-
+input.addEventListener('keyup', function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("#btn").click();
+    }
+})
 myButton.addEventListener('click', (event) => {
     i++;
-   
-    if (regex.test(input.value)){}
-        
-    else{
-         swal("Ce n'est pas un nombre valide");
-         i--;
+if (regex.test(input.value)) {} else {
+    swal("Ce n'est pas un nombre valide");
+    i--;
+    input.value = message = "";
+}
+console.log(random); console.log(myButton); console.log(i); console.log(input);
+if (input.value == random) {
+    swal("Bravo !!");
+    input.value = message.innerHTML = essais.innerHTML = "";
+    i = 0;
+} else if (input.value < random) {
+    message.innerHTML = ("C'est plus de " + input.value + "!");
+    input.value = "";
+    essais.innerHTML = ("Il te reste encore " + [10 - i] + " essais.");
+    if (i == 9) {
+        essais.innerHTML = ("Il ne te reste plus qu'un essai.");
     }
-         console.log(random);
-         console.log(myButton);
-         console.log(i);
-         console.log(input);
-         if (input.value == random) {
-             swal("Bravo !!");
-             input.value = "";
-         } else if (input.value < random) {
-             message.innerHTML = ("plus de " + input.value);
-             input.value = "";
-         } else if (input.value > random) {
-             message.innerHTML = ("moins de " + input.value);
-             input.value = "";
-         }
-         if (i == 10) {
-             swal("Perdu !!");
-             i = 0;
-         }
-     })
+} else if (input.value > random) {
+    message.innerHTML = ("C'est moins de " + input.value + "!");
+    input.value = "";
+    essais.innerHTML = ("Il te reste encore " + [10 - i] + " essais.");
+    if (i == 9) {
+        essais.innerHTML = ("Il ne te reste plus qu'un essai.");
+    }
+}
+if (i == 10) {
+    swal("Perdu !!");
+    input.value = message.innerHTML = essais.innerHTML = "";
+    i = 0;
+}
+})
