2382ecdfbd36e97d11c95faccfbd941a5c5fa385
abdelkrimn <abdelkrim.n@codeur.online>
2018-10-10 15:25:46 +0200

    [HTML,JS]
    validation enter

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let randomN = randomNumber(0,100);
+let random = randomNumber(0, 100);
 const myButton = document.querySelector('#btn');
 const message = document.querySelector('#message');
 const essais = document.querySelector("#essais");
@@ -8,49 +8,59 @@ const myReset = document.querySelector('#reset');
 let i = 0;
 let regex = /^\d+$/;
 
+
 function randomNumber(min, max) {
     return Math.round(Math.random() * (max - min) + min);
-  }
+}
 
-input.addEventListener('keyup', function (event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementById("#btn").click();
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+        jeu();
     }
 })
+
+
 myButton.addEventListener('click', (event) => {
-    i++;
-if (regex.test(input.value)) {} else {
-    swal("Ce n'est pas un nombre valide");
-    i--;
-    input.value = message = "";
-}
-console.log(random); console.log(myButton); console.log(i); console.log(input);
-if (input.value == random) {
-    swal("Bravo !!");
-    input.value = message.innerHTML = essais.innerHTML = "";
-    i = 0;
-} else if (input.value < random) {
-    message.innerHTML = ("C'est plus de " + input.value + "!");
-    input.value = "";
-    essais.innerHTML = ("Il te reste encore " + [10 - i] + " essais.");
-    if (i == 9) {
-        essais.innerHTML = ("Il ne te reste plus qu'un essai.");
-    }
-} else if (input.value > random) {
-    message.innerHTML = ("C'est moins de " + input.value + "!");
-    input.value = "";
-    essais.innerHTML = ("Il te reste encore " + [10 - i] + " essais.");
-    if (i == 9) {
-        essais.innerHTML = ("Il ne te reste plus qu'un essai.");
-    }
-}
-if (i == 10) {
-    swal("Perdu !!");
-    input.value = message.innerHTML = essais.innerHTML = "";
-    i = 0;
-}
+        jeu();
 })
+
+function jeu() {
+    i++;
+    if (regex.test(input.value)) {} else {
+        swal("Ce n'est pas un nombre valide");
+        i--;
+        input.value = message = "";
+    }
+    console.log(random);
+    console.log(myButton);
+    console.log(i);
+    console.log(input);
+    if (input.value == random) {
+        swal("Bravo !!");
+        input.value = message.innerHTML = essais.innerHTML = "";
+        i = 0;
+    } else if (input.value < random) {
+        message.innerHTML = ("C'est plus de " + input.value + "!");
+        input.value = "";
+        essais.innerHTML = ("Il te reste encore " + [10 - i] + " essais.");
+        if (i == 9) {
+            essais.innerHTML = ("Il ne te reste plus qu'un essai.");
+        }
+    } else if (input.value > random) {
+        message.innerHTML = ("C'est moins de " + input.value + "!");
+        input.value = "";
+        essais.innerHTML = ("Il te reste encore " + [10 - i] + " essais.");
+        if (i == 9) {
+            essais.innerHTML = ("Il ne te reste plus qu'un essai.");
+        }
+    }
+    if (i == 10) {
+        swal("Perdu !!");
+        input.value = message.innerHTML = essais.innerHTML = "";
+        i = 0;
+    }
+}
 
 myReset.addEventListener('click', (event) => {
     window.location.reload();
