3a99976905967c158b0c7cf879b36edef1e833d9
abdelkrimn <abdelkrim.n@codeur.online>
2018-10-10 13:43:13 +0200

    [HTML, JS]
    reset

--- app.js
+++ app.js
@@ -1,37 +1,59 @@
-const random = Math.floor(Math.random() * 100);
+let randomN = randomNumber(0,100);
 const myButton = document.querySelector('.btn');
-var message = document.querySelector('#message');
-var input = document.querySelector('#answer');
-var i = 0;
+const message = document.querySelector('#message');
+const input = document.querySelector('#answer');
+const myReset = document.querySelector('#reset');
 
 
- var regex = /^\d+$/;
+let i = 0;
+
+function randomNumber(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  }
+
+  
+const regex = /^\d+$/;
+
+function pressEnter(event) {
+    var code = eventkeyCode; //Selon le navigateur c'est which ou keyCode
+    if (code == 13) { //le code de la touche Enter
+        document.getElementById('answer').submit();
+    }
+}
+window.onload = () => {
+    input.focus();
+}
 
 myButton.addEventListener('click', (event) => {
     i++;
-   
-    if (regex.test(input.value)){}
-        
-    else{
-         swal("Ce n'est pas un nombre valide");
-         i--;
+
+    if (regex.test(input.value)) {} else {
+        swal("Ce n'est pas un nombre valide");
+        i--;
+        input.value = message = "";
     }
-         console.log(random);
-         console.log(myButton);
-         console.log(i);
-         console.log(input);
-         if (input.value == random) {
-             swal("Bravo !!");
-             input.value = "";
-         } else if (input.value < random) {
-             message.innerHTML = ("plus de " + input.value);
-             input.value = "";
-         } else if (input.value > random) {
-             message.innerHTML = ("moins de " + input.value);
-             input.value = "";
-         }
-         if (i == 10) {
-             swal("Perdu !!");
-             i = 0;
-         }
-     })
+    console.log(randomN);
+    console.log(myButton);
+    console.log(i);
+    console.log(input);
+    if (input.value == randomN) {
+        swal("Bravo !!");
+        input.value = message.innerHTML = "";
+        i = 0;
+    } else if (input.value < randomN) {
+        message.innerHTML = ("plus de " + input.value);
+        input.value = "";
+    } else if (input.value > randomN) {
+        message.innerHTML = ("moins de " + input.value);
+        input.value = "";
+    }
+    if (i == 3) {
+        swal("Perdu !!");
+        i = 0;
+        input.value = message = "";
+    }
+})
+
+myReset.addEventListener('click', (event) => {
+    window.location.reload();
+})
